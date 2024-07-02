f56a293d2fe82dfb02b01100be0c44057e1a6315
Marshall Kurniawan <marshall.kurniawan@unitcode.org>
2024-07-02 21:41:25 +0700

    chore: setup eslint, seo, tailwind, & shadcn

---
+++ src/constant/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+    title: "Unit Creative",
+    description:
+        "We are a collective of passionate designers & developers committed to bringing your branding vision to life with unforgettable designs.",
+    url: "https://www.unitcreative.com",
+};
