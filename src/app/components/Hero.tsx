8d46f792c69c45a59661920f1fdbbc43b786856a
Marshall Kurniawan <marshall.kurniawan@unitcode.org>
2024-07-05 12:10:19 +0700

    feat: adjust hero section for tab view

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -18,14 +18,14 @@ const Hero = forwardRef<HTMLDivElement, Props>(
     return (
       <section
         className={cn(
-          "bg-hero flex w-full max-w-screen-2xl flex-col-reverse overflow-hidden bg-center bg-no-repeat px-8 pb-12 pt-32 sm:px-16 sm:pt-36 lg:min-h-screen lg:pb-0 mb-24",
+          "lg:bg-hero flex w-full max-w-screen-2xl flex-col-reverse overflow-hidden bg-center bg-no-repeat px-8 pb-12 pt-32 sm:px-16 lg:pt-36 min-h-screen lg:pb-0 mb-24 gap-y-12",
           className,
         )}
         ref={ref}
         {...props}
       >
-        <div className="flex items-center justify-between">
-          <p className="w-96 text-lg">
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-y-12">
+          <p className="lg:w-96 text-lg text-center lg:text-left">
             We are a collective of passionate designers & developers committed
             to bringing your branding vision to life with unforgettable designs.
           </p>
@@ -43,7 +43,7 @@ const Hero = forwardRef<HTMLDivElement, Props>(
             Start your journey
           </Button>
         </div>
-        <div className="z-20 my-auto flex items-center justify-between font-poppins text-6xl">
+        <div className="z-20 grow lg:my-auto flex flex-col lg:flex-row items-center justify-between font-poppins text-6xl bg-hero bg-no-repeat bg-center lg:bg-none">
           <p>Where Vision</p>
           <p>Meets Reality</p>
         </div>
